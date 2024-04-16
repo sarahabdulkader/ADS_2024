295069aea804d113ea19c7955e950517fc6075b1
sarahabdulkader <sarah.abdul.kader@univie.ac.at>
2024-04-16 15:53:20 +0200

    My changes

--- 03_lecture/01strings.py
+++ 03_lecture/01strings.py
@@ -1,3 +1,4 @@
+
 """
 STRING OPERATIONS
 
@@ -15,8 +16,8 @@ The string operand is then repeated the number of times specified by the integer
 For example this would work:
 """
 
-# word = "banana"
-# print(word*3)   # prints: bananabananabanana
+#word = "banana"
+#print(word*3)   # prints: bananabananabanana
 
 """
 Using string operations together with a loop we can write a program which draws a pyramid.
@@ -90,9 +91,9 @@ You can also use negative indexing to access characters counting from the end of
 The last character in a string is at index -1, the second to last character is at index -2, and so forth. 
 You can think of input_string[-1] as shorthand for input_string[len(input_string) - 1].
 """
-# input_string = input("Please type in a string: ")
-# print("First character: " + input_string[0])
-# print("Last character: " + input_string[-1])
+input_string = input("Please type in a string: ")
+print("First character: " + input_string[0])
+print("Last character: " + input_string[-1])
 
 """
 IndexError: string index out of range
